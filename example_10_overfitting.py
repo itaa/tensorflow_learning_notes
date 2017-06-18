e13d9395d66bda4fef3581f62cfdbfe5166880a4
iccccing@gmail.com <iccccing@gmail.com>
2017-06-18 16:48:41 +0800

    Add some examples. ---Overfitting

--- example_10_overfitting.py
+++ example_10_overfitting.py
@@ -61,6 +61,8 @@ def add_layer(inputs, in_size, out_size, layer_name, activation_function=None):
         # W * x + b
         with tf.name_scope('Wx_plus_b'):
             Wx_plus_b = tf.add(tf.matmul(inputs, Weights), biases)
+            # add drop out function
+            Wx_plus_b = tf.nn.dropout(Wx_plus_b, keep_prob=keep_prob)
         # 如果activation_function是空的时候就表示是一个线性关系直接放回即可
         if activation_function is None:
             outputs = Wx_plus_b
@@ -75,8 +77,17 @@ def add_layer(inputs, in_size, out_size, layer_name, activation_function=None):
 xs = tf.placeholder(tf.float32, [None, 64])
 ys = tf.placeholder(tf.float32, [None, 10])
 
+# drop define
+# keep_prob 是你至少需要保持的连接比例，不被drop的比例
+keep_prob = tf.placeholder(tf.float32)
+
+
 # add output layer
+# hidden layer
+# 这里使用tanh可以防止报错
 l1 = add_layer(xs, 64, 100, 'l1', activation_function=tf.nn.tanh)
+# output layer
+#
 prediction = add_layer(l1, 100, 10, 'l2', activation_function=tf.nn.softmax)
 
 # the error between prediction and real data
@@ -88,4 +99,31 @@ tf.summary.scalar('loss', cross_entropy)
 train_step = tf.train.GradientDescentOptimizer(0.6).minimize(cross_entropy)
 
 sess = tf.Session()
+merged = tf.summary.merge_all()
+
+# summary writer goes in here
+
+train_writer = tf.summary.FileWriter("logs/train", sess.graph)
+test_writer = tf.summary.FileWriter("logs/test", sess.graph)
+
+# init variables
+sess.run(tf.global_variables_initializer())
+
+for i in range(500):
+    # 训练的时候drop 40%
+    sess.run(train_step, feed_dict={xs: x_train, ys: y_train, keep_prob: 0.6})
+    if i % 50 is 0:
+        # record loss
+        # 记录的时候不drop
+        train_result = sess.run(merged, feed_dict={xs: x_train, ys: y_train, keep_prob: 1})
+        test_result = sess.run(merged, feed_dict={xs: x_test, ys: y_test,  keep_prob: 1})
+
+        # writer file
+        train_writer.add_summary(train_result, i)
+        test_writer.add_summary(test_result, i)
+
+        # ps
+        # tensorboard 当中要有 history summary
+        #
+
 
